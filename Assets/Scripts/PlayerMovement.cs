239cbaed7df846ee8dcb174c4163aafe0aef38b2
Jake G <mamagotbig@gmail.com>
2026-01-16 00:03:49 +1300

    8.1 added movement system to the player within 2d space

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,18 +3,24 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
+    [SerializeField] float controlSpeed = 3f;
+
+    Vector2 movement;
 
     void Update()
     {
-        
+        ProcessTranslation();
     }
-
+    
     public void OnMove(InputValue value)
     {
-        Debug.Log(value.Get<Vector2>());
+        movement = value.Get<Vector2>();
+    }
+    private void ProcessTranslation()
+    {
+        float xOffSet = movement.x * controlSpeed * Time.deltaTime;
+        float yOffSet = movement.y * controlSpeed * Time.deltaTime;
+
+        transform.localPosition = new Vector3(transform.localPosition.x + xOffSet, transform.localPosition.y + yOffSet, 0f);
     }
 }
