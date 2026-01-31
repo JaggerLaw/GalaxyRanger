b317059dbacc58c39af9ce7a9d2d05b1e7b142f2
Jake G <mamagotbig@gmail.com>
2026-02-01 00:30:43 +1300

    16 added explosion VFX to enemy ship if destroyed

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,12 +3,14 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     int enemyHealth = 100;
+    [SerializeField] GameObject explosionVFX;
     void OnParticleCollision(GameObject other)
     {
         if(enemyHealth <= 0)
         {    
             //enemy obects that collide with the player lasers
             Destroy(this.gameObject);
+            Instantiate(explosionVFX, transform.position, Quaternion.identity);
         }
         enemyHealth -= 10;
     }
