e0805cdb120519b4ac73bd74898f496bff58af51
Jake G <mamagotbig@gmail.com>
2026-02-06 23:53:32 +1300

    19 proportioning enemy health to match size of enemy, cleaning up enemy script code

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,16 +2,21 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    int enemyHealth = 100;
+    [SerializeField] int enemyHealth = 3;
     [SerializeField] GameObject explosionVFX;
     void OnParticleCollision(GameObject other)
     {
-        if(enemyHealth <= 0)
-        {    
+        ProcessHit();
+    }
+
+    void ProcessHit()
+    {
+        if (enemyHealth <= 0)
+        {
             //enemy obects that collide with the player lasers
             Destroy(this.gameObject);
             Instantiate(explosionVFX, transform.position, Quaternion.identity);
         }
-        enemyHealth -= 10;
+        enemyHealth--;
     }
 }
